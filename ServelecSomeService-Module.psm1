6ffd647ec3948ac8a49794a0898fc33e6fe6b142
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2019-10-10 19:37:02 +0100

    Improved xml parsing

--- ServelecSomeService-Module.psm1
+++ ServelecSomeService-Module.psm1
@@ -6,13 +6,39 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     Break
 }
 
-$xdoc = new-object System.Xml.XmlDocument
-
-$file = resolve-path(".\LogicalTest.ServiceProcess.config")
-
-$xdoc.load($file)
+param (
+    [Parameter(Mandatory=$true)][string]$systemConnectionName,
+    [Parameter(Mandatory=$true)][string]$systemConnectionString,
+    [Parameter(Mandatory=$true)][string]$globalConnectionName,
+    [Parameter(Mandatory=$true)][string]$globalConnectionString,
+    [Parameter(Mandatory=$true)][string]$monitoredSystemName,
+    [string]$logLevel = '',
+    [string]$endpointAddress = ''
+)
 
 [xml] $xdoc = get-content ".\LogicalTest.ServiceProcess.config"
 
-$xdoc = [xml] (get-content ".\LogicalTest.ServiceProcess.config")
+# Constructor args
+$xdoc.configuration.spring.services.objects.object.'constructor-arg'.list
 
+# System configuration - parameter mandatory
+$xdoc.configuration.SystemConnections.Connections.SystemConnection.Name             = $systemConnectionName
+$xdoc.configuration.SystemConnections.Connections.SystemConnection.ConnectionString = $systemConnectionString
+
+# Global configuration - parameter mandatory
+$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.Name             = $globalConnectionName
+$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.ConnectionString = $globalConnectionString
+$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.MonitoredSystems.MonitoredSystem.name = $monitoredSystemName
+
+# LogLevel - parameter optional
+if ($logLevel) {
+    $xdoc.configuration.log4net.root.level.value = $logLevel
+}
+
+# Endpoint  - parameter optional
+if ($endpointAddress) {
+    $xdoc.configuration.'system.serviceModel'.client.endpoint.address = $endpointAddress
+}
+
+# Save
+$xdoc.Save(".\LogicalTest.ServiceProcess.updated.config")
