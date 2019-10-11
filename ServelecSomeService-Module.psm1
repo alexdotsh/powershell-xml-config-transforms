03d20f0eb1fff8d059192fe999353a8b0bdd009b
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2019-10-11 15:29:22 +0100

    WIP - improved error handling

--- ServelecSomeService-Module.psm1
+++ ServelecSomeService-Module.psm1
@@ -1,44 +1,78 @@
-param (
-    [string]$value1 = '',
-    [string]$value2 = '',
-    [Parameter(Mandatory=$true)][string]$systemConnectionName,
-    [Parameter(Mandatory=$true)][string]$systemConnectionString,
-    [Parameter(Mandatory=$true)][string]$globalConnectionName,
-    [Parameter(Mandatory=$true)][string]$globalConnectionString,
-    [Parameter(Mandatory=$true)][string]$monitoredSystemName,
-    [string]$logLevel = '',
-    [string]$endpointAddress = ''
-)
+function Start-Config {
+    Set-Parameter
+    if (
+        $systemConnectionName -eq $null -or
+        $systemConnectionString -eq $null -or
+        $globalConnectionName -eq $null -or
+        $globalConnectionString -eq $null -or
+        $monitoredSystemName -eq $null
+
+    ) {
+        Write-Host "The mandatory variable field is empty"
+        exit
+    }
+
+    Get-XMLDocument
+    Set-ConstructorArg
+    Set-Connection
+    Set-LogLevel
+    Set-ClientCredentialType
+    Set-EndpointAddress
+    Save-XMLDocument
+}
+
+function Set-Parameter {
+    param (
+        [string]$value1 = '',
+        [string]$value2 = '',
+        [Parameter(Mandatory=$true)][string]$systemConnectionName,
+        [Parameter(Mandatory=$true)][string]$systemConnectionString,
+        [Parameter(Mandatory=$true)][string]$globalConnectionName,
+        [Parameter(Mandatory=$true)][string]$globalConnectionString,
+        [Parameter(Mandatory=$true)][string]$monitoredSystemName,
+        [string]$logLevel = '',
+        [string]$endpointAddress = ''
+    )
+
+    Set-Variable varNum -option ReadOnly -value 1
+    Set-Variable chosenType -option ReadOnly -value ''
+}
 
 # Read xml file
-[xml] $xdoc = get-content ".\LogicalTest.ServiceProcess.config"
+function Get-XMLDocument {
+    [xml] $xdoc = get-content ".\LogicalTest.ServiceProcess.updated.config"
+}
 
 # Constructor args
-$varNum = 1
-Foreach ($list in $xdoc.SelectNodes("//configuration/spring/services/objects/object/constructor-arg/list")) {
-    Foreach ($value in $list.value) {
-        $elemDef = "value" + $varNum
-        $elemVar = Get-Variable $elemDef
-        if ($elemVar.Value) {
-            $value = $elemVar.Value
+function Set-ConstructorArg {
+    #$varNum = 1
+    Foreach ($list in $xdoc.SelectNodes("//configuration/spring/services/objects/object/constructor-arg/list")) {
+        Foreach ($value in $list.value) {
+            $elemDef = "value" + $varNum
+            $elemVar = Get-Variable $elemDef
+            if ($elemVar.Value) {
+                $value = $elemVar.Value
+            }
+        $Script:varNum++
         }
-    $Script:varNum++
     }
 }
 
 # System configuration - parameter mandatory
-$xdoc.configuration.SystemConnections.Connections.SystemConnection.Name             = $systemConnectionName
-$xdoc.configuration.SystemConnections.Connections.SystemConnection.ConnectionString = $systemConnectionString
-
-# Global configuration - parameter mandatory
-$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.Name                                  = $globalConnectionName
-$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.ConnectionString                      = $globalConnectionString
-$xdoc.configuration.GlobalConnections.Connections.GlobalConnection.MonitoredSystems.MonitoredSystem.name = $monitoredSystemName
+function Set-Connection {
+    $xdoc.configuration.SystemConnections.Connections.SystemConnection.Name                                  = $systemConnectionName
+    $xdoc.configuration.SystemConnections.Connections.SystemConnection.ConnectionString                      = $systemConnectionString
+    $xdoc.configuration.GlobalConnections.Connections.GlobalConnection.Name                                  = $globalConnectionName
+    $xdoc.configuration.GlobalConnections.Connections.GlobalConnection.ConnectionString                      = $globalConnectionString
+    $xdoc.configuration.GlobalConnections.Connections.GlobalConnection.MonitoredSystems.MonitoredSystem.name = $monitoredSystemName
+}
 
 # LogLevel - parameter optional
-if ($logLevel) { $xdoc.configuration.log4net.root.level.value = $logLevel }
+function Set-LogLevel {
+    if ($logLevel) { $xdoc.configuration.log4net.root.level.value = $logLevel }
+}
 
-$chosenType = ''
+#$chosenType = ''
 function Set-ClientCredentialType {
     Get-ClientCredentialType 
 
@@ -95,10 +129,14 @@ function Set-HttpBinding {
     $xdoc.configuration.'system.serviceModel'.bindings.basicHttpBinding.binding.security.transport.clientCredentialType = $clientCredentialType
 }
 
-Set-ClientCredentialType
-
 # Endpoint  - parameter optional
-if ($endpointAddress) { $xdoc.configuration.'system.serviceModel'.client.endpoint.address = $endpointAddress }
+function Set-EndpointAddress {
+    if ($endpointAddress) { $xdoc.configuration.'system.serviceModel'.client.endpoint.address = $endpointAddress }
+}
 
 # Save
-$xdoc.Save(".\LogicalTest.ServiceProcess.updated.config")
+function Save-XMLDocument {
+    $xdoc.Save(".\LogicalTest.ServiceProcess.updated.config")
+}
+
+Export-ModuleMember -Function Start-Config
